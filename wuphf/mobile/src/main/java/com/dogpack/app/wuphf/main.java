2beae31092678553fe26ac70128e37a8bd34499d
Eugene Lee <eugene.lee337@gmail.com>
2014-11-15 16:11:52 -0500

    added YO (untested)

--- wuphf/mobile/src/main/java/com/dogpack/app/wuphf/main.java
+++ wuphf/mobile/src/main/java/com/dogpack/app/wuphf/main.java
@@ -7,6 +7,19 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class main extends Activity {
 
@@ -40,5 +53,22 @@ public class main extends Activity {
     public void sendMessage(View view) {
         Toast toast = Toast.makeText(this, "Message Sent!", Toast.LENGTH_SHORT);
         toast.show();
+
+        List<NameValuePair> yoParams = new ArrayList<NameValuePair>(2);
+        yoParams.add(new BasicNameValuePair("api_token", "a1001602-9b08-4783-89c6-c86f700590ec"));
+        yoParams.add(new BasicNameValuePair("username", "HIPSTERVY"));
+        postData("https://api.justyo.co/yo/", yoParams);
+    }
+
+    public void postData(String url, List<NameValuePair> params) {
+        // Create a new HttpClient and Post Header
+        HttpClient httpclient = new DefaultHttpClient();
+        HttpPost httppost = new HttpPost(url);
+
+        try {
+            httppost.setEntity(new UrlEncodedFormEntity(params));
+            HttpResponse response = httpclient.execute(httppost);
+        } catch (ClientProtocolException e) {
+        } catch (IOException e) {}
     }
 }
